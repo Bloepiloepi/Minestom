958cb14b7038162bb1f978dca1a68dc085e1cccc
TogAr2 <59421074+TogAr2@users.noreply.github.com>
2024-07-18 01:49:08 +0200

    Finish entity effects ReplaceBlock, ReplaceDisc, SetBlockProperties, SpawnParticles and SummonEntity

--- src/main/java/net/minestom/server/item/enchant/EntityEffect.java
+++ src/main/java/net/minestom/server/item/enchant/EntityEffect.java
@@ -2,18 +2,21 @@ package net.minestom.server.item.enchant;
 
 import net.kyori.adventure.nbt.BinaryTag;
 import net.kyori.adventure.nbt.CompoundBinaryTag;
+import net.kyori.adventure.nbt.NumberBinaryTag;
+import net.minestom.server.config.BlockPredicate;
+import net.minestom.server.config.BlockStateProvider;
 import net.minestom.server.config.FloatProvider;
 import net.minestom.server.coordinate.Point;
 import net.minestom.server.entity.damage.DamageType;
 import net.minestom.server.gamedata.DataPack;
 import net.minestom.server.gamedata.tags.Tag;
-import net.minestom.server.instance.block.Block;
+import net.minestom.server.item.component.ItemBlockState;
 import net.minestom.server.particle.Particle;
-import net.minestom.server.potion.PotionEffect;
 import net.minestom.server.registry.DynamicRegistry;
 import net.minestom.server.registry.ObjectSet;
 import net.minestom.server.registry.Registries;
 import net.minestom.server.sound.SoundEvent;
+import net.minestom.server.utils.NamespaceID;
 import net.minestom.server.utils.nbt.BinaryTagSerializer;
 import net.minestom.server.utils.validate.Check;
 import org.jetbrains.annotations.ApiStatus;
@@ -22,7 +25,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Objects;
-
 
 public non-sealed interface EntityEffect extends Enchantment.Effect {
 
@@ -67,7 +69,7 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
     }
 
     record ApplyPotionEffect(
-            @NotNull ObjectSet<PotionEffect> toApply,
+            @NotNull ObjectSet toApply,
             @NotNull LevelBasedValue minDuration,
             @NotNull LevelBasedValue maxDuration,
             @NotNull LevelBasedValue minAmplifier,
@@ -121,7 +123,7 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
     record Explode(
             boolean attributeToUser,
             @Nullable DynamicRegistry.Key<DamageType> damageType,
-            @Nullable ObjectSet<Block> immuneBlocks,
+            @Nullable ObjectSet immuneBlocks,
             @Nullable LevelBasedValue knockbackMultiplier,
             @Nullable Point offset,
             @NotNull LevelBasedValue radius,
@@ -131,27 +133,27 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
             @NotNull Particle largeParticle,
             @NotNull SoundEvent sound
     ) implements EntityEffect, LocationEffect {
-        private static final BinaryTagSerializer<ObjectSet<Block>> IMMUNE_BLOCKS_NBT_TYPE = ObjectSet.nbtType(Tag.BasicType.BLOCKS);
+        private static final BinaryTagSerializer<ObjectSet> IMMUNE_BLOCKS_NBT_TYPE = ObjectSet.nbtType(Tag.BasicType.BLOCKS);
         public static final BinaryTagSerializer<Explode> NBT_TYPE = new BinaryTagSerializer<>() {
             @Override
             public @NotNull BinaryTag write(@NotNull Context context, @NotNull Explode value) {
                 CompoundBinaryTag.Builder builder = CompoundBinaryTag.builder();
 
-                builder.putBoolean("attribute_to_user", value.attributeToUser);
-                if (value.damageType != null)
-                    builder.put("damage_type", DamageType.NBT_TYPE.write(context, value.damageType));
-                if (value.immuneBlocks != null)
-                    builder.put("immune_blocks", IMMUNE_BLOCKS_NBT_TYPE.write(context, value.immuneBlocks));
-                if (value.knockbackMultiplier != null)
-                    builder.put("knockback_multiplier", LevelBasedValue.NBT_TYPE.write(context, value.knockbackMultiplier));
-                if (value.offset != null)
-                    builder.put("offset", BinaryTagSerializer.BLOCK_POSITION.write(context, value.offset));
-                builder.put("radius", LevelBasedValue.NBT_TYPE.write(context, value.radius));
+                builder.putBoolean("attribute_to_user", value.attributeToUser());
+                if (value.damageType() != null)
+                    builder.put("damage_type", DamageType.NBT_TYPE.write(context, value.damageType()));
+                if (value.immuneBlocks() != null)
+                    builder.put("immune_blocks", IMMUNE_BLOCKS_NBT_TYPE.write(context, value.immuneBlocks()));
+                if (value.knockbackMultiplier() != null)
+                    builder.put("knockback_multiplier", LevelBasedValue.NBT_TYPE.write(context, value.knockbackMultiplier()));
+                if (value.offset() != null)
+                    builder.put("offset", BinaryTagSerializer.BLOCK_POSITION.write(context, value.offset()));
+                builder.put("radius", LevelBasedValue.NBT_TYPE.write(context, value.radius()));
                 builder.putBoolean("create_fire", value.createFire);
-                builder.putString("block_interaction", value.blockInteraction.id());
-                builder.put("small_particle", Particle.NBT_TYPE.write(context, value.smallParticle));
-                builder.put("large_particle", Particle.NBT_TYPE.write(context, value.largeParticle));
-                builder.put("sound", SoundEvent.NBT_TYPE.write(value.sound));
+                builder.put("block_interaction", BlockInteraction.NBT_TYPE.write(value.blockInteraction()));
+                builder.put("small_particle", Particle.NBT_TYPE.write(context, value.smallParticle()));
+                builder.put("large_particle", Particle.NBT_TYPE.write(context, value.largeParticle()));
+                builder.put("sound", SoundEvent.NBT_TYPE.write(value.sound()));
 
                 return builder.build();
             }
@@ -160,18 +162,18 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
             public @NotNull Explode read(@NotNull Context context, @NotNull BinaryTag tag) {
                 final CompoundBinaryTag compound = (CompoundBinaryTag) tag;
 
-                boolean attributeToUser = compound.getBoolean("attribute_to_user");
+                boolean attributeToUser = compound.getBoolean("attribute_to_user", false);
                 BinaryTag damageTypeTag = compound.get("damage_type");
                 DynamicRegistry.Key<DamageType> damageType = damageTypeTag == null ? null : DamageType.NBT_TYPE.read(context, damageTypeTag);
                 BinaryTag immuneBlocksTag = compound.get("immune_blocks");
-                ObjectSet<Block> immuneBlocks = immuneBlocksTag == null ? null : IMMUNE_BLOCKS_NBT_TYPE.read(context, immuneBlocksTag);
+                ObjectSet immuneBlocks = immuneBlocksTag == null ? null : IMMUNE_BLOCKS_NBT_TYPE.read(context, immuneBlocksTag);
                 BinaryTag knockbackMultiplierTag = compound.get("knockback_multiplier");
                 LevelBasedValue knockbackMultiplier = knockbackMultiplierTag == null ? null : LevelBasedValue.NBT_TYPE.read(context, knockbackMultiplierTag);
                 BinaryTag offsetTag = compound.get("offset");
                 Point offset = offsetTag == null ? null : BinaryTagSerializer.BLOCK_POSITION.read(context, offsetTag);
                 LevelBasedValue radius = LevelBasedValue.NBT_TYPE.read(context, Objects.requireNonNull(compound.get("radius")));
-                boolean createFire = compound.getBoolean("create_fire");
-                BlockInteraction blockInteraction = BlockInteraction.fromId(compound.getString("block_interaction"));
+                boolean createFire = compound.getBoolean("create_fire", false);
+                BlockInteraction blockInteraction = BlockInteraction.NBT_TYPE.read(Objects.requireNonNull(compound.get("block_interaction")));
                 Particle smallParticle = Particle.NBT_TYPE.read(context, Objects.requireNonNull(compound.get("small_particle")));
                 Particle largeParticle = Particle.NBT_TYPE.read(context, Objects.requireNonNull(compound.get("large_particle")));
                 SoundEvent sound = SoundEvent.NBT_TYPE.read(context, Objects.requireNonNull(compound.get("sound")));
@@ -191,31 +193,13 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
         }
 
         public enum BlockInteraction {
-            NONE("none"),
-            BLOCK("block"),
-            MOB("mob"),
-            TNT("tnt"),
-            TRIGGER("trigger");
+            NONE,
+            BLOCK,
+            MOB,
+            TNT,
+            TRIGGER;
 
-            private final String id;
-
-            BlockInteraction(String id) {
-                this.id = id;
-            }
-
-            public String id() {
-                return id;
-            }
-
-            public static @NotNull BlockInteraction fromId(String id) {
-                for (BlockInteraction blockInteraction : values()) {
-                    if (blockInteraction.id.equals(id)) {
-                        return blockInteraction;
-                    }
-                }
-
-                return NONE;
-            }
+            public static final BinaryTagSerializer<BlockInteraction> NBT_TYPE = BinaryTagSerializer.fromEnumStringable(BlockInteraction.class);
         }
     }
 
@@ -250,12 +234,18 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
     }
 
     record ReplaceBlock(
-            CompoundBinaryTag content
-//            Object blockState, // "A block state provider giving the block state to set"
-//            @NotNull Point offset,
-//            @Nullable Object predicate // "A World-generation style Block Predicate to used to determine if the block should be replaced"
+            @NotNull BlockStateProvider blockState,
+            @Nullable Point offset,
+            @Nullable BlockPredicate predicate,
+            @Nullable NamespaceID triggerGameEvent
     ) implements EntityEffect, LocationEffect {
-        public static final BinaryTagSerializer<ReplaceBlock> NBT_TYPE = BinaryTagSerializer.COMPOUND.map(ReplaceBlock::new, ReplaceBlock::content);
+        public static final BinaryTagSerializer<ReplaceBlock> NBT_TYPE = BinaryTagSerializer.object(
+                "block_state", BlockStateProvider.NBT_TYPE, ReplaceBlock::blockState,
+                "offset", BinaryTagSerializer.BLOCK_POSITION, ReplaceBlock::offset,
+                "predicate", BlockPredicate.NBT_TYPE, ReplaceBlock::predicate,
+                "trigger_game_event", BinaryTagSerializer.STRING.map(NamespaceID::from, NamespaceID::asString), ReplaceBlock::triggerGameEvent,
+                ReplaceBlock::new
+        );
 
         @Override
         public @NotNull BinaryTagSerializer<ReplaceBlock> nbtType() {
@@ -264,10 +254,22 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
     }
 
     record ReplaceDisc(
-            CompoundBinaryTag content
-            // todo
+            @NotNull BlockStateProvider blockState,
+            @NotNull LevelBasedValue radius,
+            @NotNull LevelBasedValue height,
+            @Nullable Point offset,
+            @Nullable BlockPredicate predicate,
+            @Nullable NamespaceID triggerGameEvent
     ) implements EntityEffect, LocationEffect {
-        public static final BinaryTagSerializer<ReplaceDisc> NBT_TYPE = BinaryTagSerializer.COMPOUND.map(ReplaceDisc::new, ReplaceDisc::content);
+        public static final BinaryTagSerializer<ReplaceDisc> NBT_TYPE = BinaryTagSerializer.object(
+                "block_state", BlockStateProvider.NBT_TYPE, ReplaceDisc::blockState,
+                "radius", LevelBasedValue.NBT_TYPE, ReplaceDisc::radius,
+                "height", LevelBasedValue.NBT_TYPE, ReplaceDisc::height,
+                "offset", BinaryTagSerializer.BLOCK_POSITION, ReplaceDisc::offset,
+                "predicate", BlockPredicate.NBT_TYPE, ReplaceDisc::predicate,
+                "trigger_game_event", BinaryTagSerializer.STRING.map(NamespaceID::from, NamespaceID::asString), ReplaceDisc::triggerGameEvent,
+                ReplaceDisc::new
+        );
 
         @Override
         public @NotNull BinaryTagSerializer<ReplaceDisc> nbtType() {
@@ -290,10 +292,16 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
     }
 
     record SetBlockProperties(
-            CompoundBinaryTag content
-            //todo
+            @NotNull ItemBlockState properties,
+            @Nullable Point offset,
+            @Nullable NamespaceID triggerGameEvent
     ) implements EntityEffect, LocationEffect {
-        public static final BinaryTagSerializer<SetBlockProperties> NBT_TYPE = BinaryTagSerializer.COMPOUND.map(SetBlockProperties::new, SetBlockProperties::content);
+        public static final BinaryTagSerializer<SetBlockProperties> NBT_TYPE = BinaryTagSerializer.object(
+                "properties", ItemBlockState.NBT_TYPE, SetBlockProperties::properties,
+                "offset", BinaryTagSerializer.BLOCK_POSITION, SetBlockProperties::offset,
+                "trigger_game_event", BinaryTagSerializer.STRING.map(NamespaceID::from, NamespaceID::asString), SetBlockProperties::triggerGameEvent,
+                SetBlockProperties::new
+        );
 
         @Override
         public @NotNull BinaryTagSerializer<SetBlockProperties> nbtType() {
@@ -302,22 +310,110 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
     }
 
     record SpawnParticles(
-            CompoundBinaryTag content
-            //todo
+            @NotNull Particle particle,
+            @NotNull PositionSource horizontalPosition,
+            @NotNull PositionSource verticalPosition,
+            @NotNull VelocitySource horizontalVelocity,
+            @NotNull VelocitySource verticalVelocity,
+            @Nullable FloatProvider speed
     ) implements EntityEffect, LocationEffect {
-        public static final BinaryTagSerializer<SpawnParticles> NBT_TYPE = BinaryTagSerializer.COMPOUND.map(SpawnParticles::new, SpawnParticles::content);
+        public static final BinaryTagSerializer<SpawnParticles> NBT_TYPE = BinaryTagSerializer.object(
+                "particle", Particle.NBT_TYPE, SpawnParticles::particle,
+                "horizontal_position", PositionSource.NBT_TYPE, SpawnParticles::horizontalPosition,
+                "vertical_position", PositionSource.NBT_TYPE, SpawnParticles::verticalPosition,
+                "horizontal_velocity", VelocitySource.NBT_TYPE, SpawnParticles::horizontalVelocity,
+                "vertical_velocity", VelocitySource.NBT_TYPE, SpawnParticles::verticalVelocity,
+                "speed", FloatProvider.NBT_TYPE, SpawnParticles::speed,
+                SpawnParticles::new
+        );
 
         @Override
         public @NotNull BinaryTagSerializer<SpawnParticles> nbtType() {
             return NBT_TYPE;
         }
+
+        public record PositionSource(@NotNull PositionSource.Type type, float offset, float scale) {
+            public static final BinaryTagSerializer<PositionSource> NBT_TYPE = new BinaryTagSerializer<>() {
+                @Override
+                public @NotNull BinaryTag write(@NotNull Context context, @NotNull PositionSource value) {
+                    CompoundBinaryTag.Builder builder = CompoundBinaryTag.builder();
+                    builder.put("type", Type.NBT_TYPE.write(context, value.type()));
+                    if (value.offset() != 0) builder.putFloat("offset", value.offset());
+                    if (value.scale() != 1) builder.putFloat("scale", value.scale());
+                    return builder.build();
+                }
+
+                @Override
+                public @NotNull PositionSource read(@NotNull Context context, @NotNull BinaryTag tag) {
+                    if (!(tag instanceof CompoundBinaryTag compound))
+                        throw new IllegalArgumentException("Compound expected for position source");
+                    Type type = Type.NBT_TYPE.read(context, Objects.requireNonNull(compound.get("type")));
+                    BinaryTag offsetTag = compound.get("offset");
+                    float offset = offsetTag instanceof NumberBinaryTag number ? number.floatValue() : 0f;
+                    BinaryTag scaleTag = compound.get("scale");
+                    float scale = scaleTag instanceof NumberBinaryTag number ? number.floatValue() : 1f;
+
+                    return new PositionSource(type, offset, scale);
+                }
+            };
+
+            public enum Type {
+                ENTITY_POSITION,
+                IN_BOUNDING_BOX;
+
+                public static final BinaryTagSerializer<Type> NBT_TYPE = BinaryTagSerializer.fromEnumStringable(Type.class);
+            }
+        }
+
+        public record VelocitySource(@Nullable FloatProvider base, float movementScale) {
+            public static final BinaryTagSerializer<VelocitySource> NBT_TYPE = new BinaryTagSerializer<>() {
+                @Override
+                public @NotNull BinaryTag write(@NotNull Context context, @NotNull VelocitySource value) {
+                    CompoundBinaryTag.Builder builder = CompoundBinaryTag.builder();
+                    if (value.base() != null) builder.put("base", FloatProvider.NBT_TYPE.write(context, value.base()));
+                    if (value.movementScale() != 0) builder.putFloat("movement_scale", value.movementScale());
+                    return builder.build();
+                }
+
+                @Override
+                public @NotNull VelocitySource read(@NotNull Context context, @NotNull BinaryTag tag) {
+                    if (!(tag instanceof CompoundBinaryTag compound))
+                        throw new IllegalArgumentException("Compound expected for velocity source");
+                    BinaryTag baseTag = compound.get("base");
+                    FloatProvider base = baseTag instanceof CompoundBinaryTag baseCompound ? FloatProvider.NBT_TYPE.read(context, baseCompound) : null;
+                    BinaryTag movementScaleTag = compound.get("movement_scale");
+                    float movementScale = movementScaleTag instanceof NumberBinaryTag number ? number.floatValue() : 0f;
+
+                    return new VelocitySource(base, movementScale);
+                }
+            };
+        }
     }
 
     record SummonEntity(
-            CompoundBinaryTag content
-            //todo
+            @NotNull ObjectSet entity,
+            boolean joinTeam
     ) implements EntityEffect, LocationEffect {
-        public static final BinaryTagSerializer<SummonEntity> NBT_TYPE = BinaryTagSerializer.COMPOUND.map(SummonEntity::new, SummonEntity::content);
+        private static final BinaryTagSerializer<ObjectSet> ENTITY_NBT_TYPE = ObjectSet.nbtType(Tag.BasicType.ENTITY_TYPES);
+        public static final BinaryTagSerializer<SummonEntity> NBT_TYPE = new BinaryTagSerializer<>() {
+            @Override
+            public @NotNull BinaryTag write(@NotNull Context context, @NotNull SummonEntity value) {
+                return CompoundBinaryTag.builder()
+                        .put("entity", ENTITY_NBT_TYPE.write(value.entity()))
+                        .putBoolean("join_team", value.joinTeam())
+                        .build();
+            }
+
+            @Override
+            public @NotNull SummonEntity read(@NotNull Context context, @NotNull BinaryTag tag) {
+                if (!(tag instanceof CompoundBinaryTag compound))
+                    throw new IllegalArgumentException("Compound expected for summon entity");
+                return new SummonEntity(
+                        ENTITY_NBT_TYPE.read(Objects.requireNonNull(compound.get("entity"))),
+                        compound.getBoolean("join_team", false)
+                );
+            }
+        };
 
         @Override
         public @NotNull BinaryTagSerializer<SummonEntity> nbtType() {
